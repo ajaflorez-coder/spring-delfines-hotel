df7dc0275d4d2401ba3a21dddbb3cae61fe94a7f
RomeroValverdeFL <luizfrv1011@gmail.com>
2019-12-09 16:35:30 -0500

    Entidades

--- src/main/java/pe/edu/delfines/models/entity/Alquiler.java
+++ src/main/java/pe/edu/delfines/models/entity/Alquiler.java
@@ -1,10 +1,15 @@
 package pe.edu.delfines.models.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "alquileres")
@@ -13,6 +18,130 @@ public class Alquiler {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "precio")
+	private Float precio;
+	
+	@Column(name = "fecha_entrada", length = 60)
+	private String fechaEntrada;
+	
+	@Column(name = "fecha_salida", length = 60)
+	private String fechaSalida;
+	
+	@Column(name = "estado", length = 60)
+	private String estado;
+
+	
+	@Column(name = "observacion", length = 250)
+	private String observacion;
+	
+	
+	@JsonIgnoreProperties("alquileres")
+	@ManyToOne
+	@JoinColumn(name = "vendedor_id")
+	private Vendedor vendedor;
+	
+	
+	@JsonIgnoreProperties("alquileres")
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+	
+	
+	@JsonIgnoreProperties("alquileres")
+	@ManyToOne
+	@JoinColumn(name = "habitacion_id")
+	private Habitacion habitacion;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Float getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+
+
+	public String getFechaEntrada() {
+		return fechaEntrada;
+	}
+
+
+	public void setFechaEntrada(String fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+
+
+	public String getFechaSalida() {
+		return fechaSalida;
+	}
+
+
+	public void setFechaSalida(String fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+
+
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
 	
 	
 }
