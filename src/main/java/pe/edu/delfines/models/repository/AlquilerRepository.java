4befc45bc1b43440f9d356d6484a3c42cc9c4374
ronin45capc19 <beto12-u1@hotmail.com>
2019-12-09 19:08:25 -0500

    Subiendo

--- src/main/java/pe/edu/delfines/models/repository/AlquilerRepository.java
+++ src/main/java/pe/edu/delfines/models/repository/AlquilerRepository.java
@@ -3,9 +3,10 @@ package pe.edu.delfines.models.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import pe.edu.delfines.models.entity.Habitacion;
+import pe.edu.delfines.models.entity.Alquiler;
+import pe.edu.delfines.models.entity.Cliente;
 
 @Repository
-public interface HabitacionRepository extends JpaRepository<Habitacion, Integer> {
-	
+public interface AlquilerRepository extends JpaRepository<Alquiler, Integer>{
+
 }
